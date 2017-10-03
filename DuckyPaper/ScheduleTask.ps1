5e81e55a75f356d4ba5d94c7528b9dde534d0969
Christopher Bown <cbown75@gmail.com>
2017-10-02 21:47:30 -0700

    Initial Commit

---
+++ DuckyPaper/ScheduleTask.ps1
@@ -0,0 +1,19 @@
+﻿function ScheduleScript()
+{
+    $repeat = (New-TimeSpan -Minutes 15)
+    $jobname = "DonWebster"
+    $script =  "D:\Downloads\DuckyPaper\duckypaper.ps1"
+
+    Unregister-ScheduledTask -TaskName $jobname -Confirm:$false
+
+    $scriptblock = [scriptblock]::Create($script)
+
+    Write-Host $scriptblock
+
+    $action = New-ScheduledTaskAction -Execute 'Powershell.exe' 
+    $trigger = New-JobTrigger -Once -At (Get-Date).Date -RepeatIndefinitely -RepetitionInterval $repeat
+    $options = New-ScheduledJobOption -RunElevated -ContinueIfGoingOnBattery -StartIfOnBattery
+    Register-ScheduledJob -Name $jobname -ScriptBlock $scriptblock -Trigger $trigger -ScheduledJobOption $options
+}
+
+ScheduleScript
